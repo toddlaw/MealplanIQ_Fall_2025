4e826f041979f2aaa94c133c5fba4697354f8537
Diane Choi <ekska21@gmail.com>
2024-05-07 19:20:48 -0700

    Refactor create_charge() and add subscription test function

--- backend/app/payment_stripe.py
+++ backend/app/payment_stripe.py
@@ -5,18 +5,36 @@ from app import app
 
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 
-@app.route('/create-charge', methods=['POST'])
-def create_charge():
+def create_charge(token, amount):
     try:
-        token = request.json.get('token')
-        amount = request.json.get('amount')
-
         charge = stripe.Charge.create(
-            amount=amount,
+            amount=amount,  # amount in cents
             currency='usd',
             description='Example charge',
             source=token
         )
         return jsonify(charge), 200
+    except stripe.error.StripeError as e:
+        return jsonify(error=str(e)), 400
+    
+def create_subscription(email, token, plan_id):
+    # if plan not in ['yearly', 'monthly']:
+    #     return jsonify(error='Invalid plan'), 400
+    # plan_id = os.getenv(f'STRIPE_{plan.upper()}_SUBSCRIPTION_ID')
+
+    try:
+        customer = stripe.Customer.create(
+            email=email,
+            source=token
+        )
+        subscription = stripe.Subscription.create(
+            customer=customer.id,
+            items=[
+                {
+                    'plan': plan_id,
+                },
+            ]
+        )
+        return jsonify(subscription), 200
     except stripe.error.StripeError as e:
         return jsonify(error=str(e)), 400
