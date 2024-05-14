3a3c6f6149d4123a57f75bd364f77524bbe771ac
Diane Choi <ekska21@gmail.com>
2024-05-13 18:55:28 -0700

    Add pricing table from script.js for subscription payment

--- frontend/src/app/components/payment/payment.component.ts
+++ frontend/src/app/components/payment/payment.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-
-declare var Stripe: any;
-// var stripe = Stripe()
 
 @Component({
   selector: 'app-payment',
@@ -11,37 +7,13 @@ declare var Stripe: any;
   styleUrls: ['./payment.component.css'],
 })
 export class PaymentComponent implements OnInit {
-  stripe: any; // Stripe instance
-  card: any; // Card instance
-  email: string = '';
-  plan_type: string = '';
+  stripePublicKey!: string;
+  stripePricingTableId!: string;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit() {
-    this.stripe = Stripe(environment.STRIPE_PUBLIC_KEY);
-    let elements = this.stripe.elements();
-    this.card = elements.create('card');
-    this.card.mount('#card-element'); // Mount the card element to a div with id 'card-element'
-  }
-
-  handleForm(e: any) {
-    e.preventDefault();
-    this.stripe.createToken(this.card).then((result: any) => {
-      if (result.error) {
-        console.log('Stripe error: ', result.error);
-      } else {
-        // Send the token to server
-        this.http
-          .post('http://localhost:5000/create-subscription', {
-            token: result.token.id,
-            email: this.email,
-            plan_type: this.plan_type,
-          })
-          .subscribe((data) => {
-            console.log('Payment succeeded: ', data);
-          });
-      }
-    });
+    this.stripePublicKey = environment.stripePublicKey;
+    this.stripePricingTableId = environment.stripePricingTableId;
   }
 }
