5704dd835b27e54cbc1515c3a16c4138d167b468
Fiona Wong <fwwong01@gmail.com>
2024-05-15 16:07:24 -0700

    Updated sign up page

--- frontend/src/app/components/sign-up/sign-up.component.ts
+++ frontend/src/app/components/sign-up/sign-up.component.ts
@@ -30,7 +30,7 @@ export function passwordsMatchValidator(): ValidatorFn {
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss'],
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
   signUpForm = this.fb.group(
