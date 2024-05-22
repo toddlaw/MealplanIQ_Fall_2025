d7decabb7dc753fb6243f17ab29ff830ddbeeebd
Fiona Wong <fwwong01@gmail.com>
2024-05-21 18:21:08 -0700

    routed the placeholder image

--- frontend/src/app/components/carousel/carousel.component.ts
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -5,14 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
+
 export class CarouselComponent implements OnInit {
 
   currentItem: number = 0;
-  totalItems: number = 4;  // Adjust this to match the number of items in the carousel
+  totalItems: number = 4; 
+
+  placeHolders = [
+    {image:'feta salad', file_name: 'assets/images/placeholders/placeholder_1.jpg'},
+    {image: 'chicken pizza', file_name: 'assets/images/placeholders/placeholder_2.jpg'},
+    {image:'berries', file_name: 'assets/images/placeholders/placeholder_3.jpg'},
+    {image: 'spinach toast', file_name: 'assets/images/placeholders/placeholder_4.jpg'},
+  ]
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.setupCarousel();
+  }
+
+  setupCarousel(): void {
+    const totalItems = this.placeHolders.length;
+    setInterval(() => {
+      this.currentItem = (this.currentItem + 1) % totalItems;
+    }, 3000);
+  }
 
   updateCarousel(index: number) {
     this.currentItem = index;
