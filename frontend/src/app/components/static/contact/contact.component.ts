6a7e29c0eb009af46529521d55718cef86bb09ec
Fiona Wong <fwwong01@gmail.com>
2024-05-22 21:19:55 -0700

    Made contact us form

--- frontend/src/app/components/static/contact/contact.component.ts
+++ frontend/src/app/components/static/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -6,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
 
-  constructor() { }
+  SubjectOptions = [
+    {'value': 'general_inquiry', 'subject': 'General Inquiry'},
+    {'value': 'support', 'subject': 'Support'},
+    {'value': 'feedback', 'subject': 'Feedback'},
+    {'value': 'opportunity', 'subject': 'Opportunity'},
+    {'value': 'other', 'subject': 'Other'},
+  ]
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required], 
+      email: ['', [Validators.required, Validators.email]], 
+      subject: ['', Validators.required], 
+      message: ['', Validators.required]
+    })
+   }
+
+   onSubmit() {
+    if (this.contactForm.valid){
+      console.log('Form submitted', this.contactForm.value);
+      //
+    } else {
+    console.log('unsucessful');
+   }
+  }
+  
   ngOnInit(): void {
   }
 
