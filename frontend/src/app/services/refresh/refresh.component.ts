55beb1958841fd9029a916f67f76dea1e391f379
Diane Choi <ekska21@gmail.com>
2024-05-28 20:20:56 -0700

    Modify http request to pass the whole meal plan

--- frontend/src/app/services/refresh/refresh.component.ts
+++ frontend/src/app/services/refresh/refresh.component.ts
@@ -2,28 +2,24 @@ import { Component, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 @Component({
   selector: 'app-refresh',
   templateUrl: './refresh.component.html',
-  styleUrls: ['./refresh.component.css']
+  styleUrls: ['./refresh.component.css'],
 })
-export class RefreshComponent{
-  private apiUrl = "http://localhost:5000/api/refresh-meal-plan";
+export class RefreshComponent {
+  private apiUrl = 'http://localhost:5000/api/refresh-meal-plan';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   refreshRecipe(recipeId: string, mealPlan: any): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const body = { recipe_id: recipeId, meal_plan: mealPlan };
-    return this.http.post<any>(this.apiUrl, body, { 
-      headers: headers 
+    return this.http.post<any>(this.apiUrl, body, {
+      headers: headers,
     });
   }
-
-
 }
