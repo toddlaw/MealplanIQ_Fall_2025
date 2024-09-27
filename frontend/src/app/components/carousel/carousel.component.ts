1f613082b5c9026adf8dd8f8f886f09d047143ce
Karen Y <cj.karen02@gmail.com>
2024-09-26 21:50:46 -0700

    feat: added carousel images, adjusted the width of the container on the landing page

--- frontend/src/app/components/carousel/carousel.component.ts
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -9,13 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselComponent implements OnInit {
 
   currentItem: number = 0;
-  totalItems: number = 4; 
+  totalItems: number = 13; 
 
   placeHolders = [
-    {image:'1', file_name: 'assets/images/placeholders/placeholder_1.jpg'},
-    {image: '2', file_name: 'assets/images/placeholders/placeholder_2.jpg'},
-    {image:'3', file_name: 'assets/images/placeholders/placeholder_3.jpg'},
-    {image: '4', file_name: 'assets/images/placeholders/placeholder_4.jpg'},
+    {image:'1', file_name: 'assets/images/placeholders/placeholder_1.png'},
+    {image: '2', file_name: 'assets/images/placeholders/placeholder_2.png'},
+    {image:'3', file_name: 'assets/images/placeholders/placeholder_3.png'},
+    {image: '4', file_name: 'assets/images/placeholders/placeholder_4.png'},
+    {image: '5', file_name: 'assets/images/placeholders/placeholder_5.png'},
+    {image: '6', file_name: 'assets/images/placeholders/placeholder_6.png'},
+    {image: '7', file_name: 'assets/images/placeholders/placeholder_7.png'},
+    {image: '8', file_name: 'assets/images/placeholders/placeholder_8.png'},
+    {image: '9', file_name: 'assets/images/placeholders/placeholder_9.png'},
+    {image: '10', file_name: 'assets/images/placeholders/placeholder_10.png'},
+    {image: '11', file_name: 'assets/images/placeholders/placeholder_11.png'},
+    {image: '12', file_name: 'assets/images/placeholders/placeholder_12.png'},
+    {image: '13', file_name: 'assets/images/placeholders/placeholder_13.png'},
   ]
 
   constructor() { }
