613d1419283ab3abe804bb32f5e6009e9d138ca2
Diane Choi <ekska21@gmail.com>
2025-03-31 01:56:32 -0700

    Refactor carousel to support dynamic layout

--- frontend/src/app/components/carousel/carousel.component.ts
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CarouselComponent implements OnInit {
-
+  @Input() imgClass: string = ''; 
+  @Input() containerClass: string = '';
+  @Input() extraClass: string = '';
   currentItem: number = 0;
   totalItems: number = 13; 
 
