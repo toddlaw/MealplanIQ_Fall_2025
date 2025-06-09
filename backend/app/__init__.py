3cd99d0322f9fe56efb56a2c8ca371bf05a22560
Julie Oh <ohjeoung5224@gmail.com>
2025-06-09 16:36:56 -0700

    Initialize database during Flask app startup in app/__init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
 from flask_apscheduler import APScheduler
 from flask_cors import CORS
+from user_db.user_db import DatabaseManager
+from user_db.initiate_db import DatabaseSchemaManager
+
 
 
 app = Flask(__name__)
@@ -8,6 +11,10 @@ app = Flask(__name__)
 scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
+
+db = DatabaseManager.connect_to_database()
+schema_manager = DatabaseSchemaManager(db)
+schema_manager.create_all_tables()
 
 CORS(app)
 from app import routes
