e05d5729c74419521403dd2ecdbc91bf528c724d
Julie Oh <ohjeoung5224@gmail.com>
2025-08-01 14:13:28 -0700

    Add response query parse

--- frontend/src/app/components/meal-plan/meal-plan.component.ts
+++ frontend/src/app/components/meal-plan/meal-plan.component.ts
@@ -8,13 +8,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MealPlanComponent implements OnInit {
   date: string | null = null;
+  response: string | null = null;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
-      this.date = params['sent_date'] || null;
+      this.date = params['date'] || null;
+      this.response = params['response'] || null;
       console.log('Date from query:', this.date);
+      console.log('response', this.response);
   });
 }
 }
