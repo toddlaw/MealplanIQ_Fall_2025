31660f740200df06edfd93c007cf516170154c22
Julie Oh <ohjeoung5224@gmail.com>
2025-08-05 10:55:26 -0700

    Add fetching service

---
+++ frontend/src/app/services/meal-plan.service.ts
@@ -0,0 +1,19 @@
+// meal-plan.service.ts
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MealPlanService {
+  static getMealPlan(path: string): any {
+    throw new Error('Method not implemented.');
+  }
+  private baseUrl = 'http://localhost:5000/api/mealplan'; 
+  constructor(private http: HttpClient) {}
+
+  getMealPlan(path: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${path}`);
+  }
+}
