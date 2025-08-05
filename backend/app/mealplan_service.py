78717e77686f28f933e3aa6081e3f1b6924ce127
Julie Oh <ohjeoung5224@gmail.com>
2025-08-05 10:52:00 -0700

    Retrieve data from gcp bucket

--- backend/app/mealplan_service.py
+++ backend/app/mealplan_service.py
@@ -13,4 +13,23 @@ def upload_mealplan_json_to_gcs(response_data, path):
         content_type='application/json'
     )
     print(f"Meal plan file is successfully uploaded to {path}")
-    
+
+def download_mealplan_json_from_gcs(mealplan_path):
+    print("[DEBUG] GCS Download Path:", mealplan_path)
+
+    if not mealplan_path.endswith(".json"):
+        mealplan_path += ".json"
+
+    try:
+        client = storage.Client.from_service_account_info(
+            json.loads(os.getenv("SERVICE_JSON"))
+        )
+        bucket = client.bucket("meal-plan-data")
+        blob = bucket.blob(mealplan_path)
+
+        data = blob.download_as_string().decode("utf-8")
+        return json.loads(data)
+
+    except Exception as e:
+        print(f"[ERROR] Failed to download from GCS: {e}")
+        raise
