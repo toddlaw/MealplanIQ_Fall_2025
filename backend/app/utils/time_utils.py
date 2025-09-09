823848f7c0cb4e25fbc199f484e18a4e07ae19c3
Julie Oh <ohjeoung5224@gmail.com>
2025-09-09 16:31:49 -0700

    Add start and end dt to set it up to 12:00

--- backend/app/utils/time_utils.py
+++ backend/app/utils/time_utils.py
@@ -15,6 +15,8 @@ def get_week_range():
     start_date = now + dt.timedelta(days=1)       
     end_date = start_date + dt.timedelta(days=6)   
     today = now.date()
+    start_dt = start_date.astimezone(PT).replace(hour=0, minute=0, second=0, microsecond=0)
+    end_dt = end_date.astimezone(PT).replace(hour=0, minute=0, second=0, microsecond=0)
 
     return {
         "now": now,
@@ -24,5 +26,7 @@ def get_week_range():
         "end_date": end_date,
         "start_str": start_date.strftime("%Y-%m-%d"),
         "end_str": end_date.strftime("%Y-%m-%d"),
-        "today_str": today.strftime("%Y-%m-%d")
+        "today_str": today.strftime("%Y-%m-%d"),
+        "start_dt": start_dt,
+        "end_dt": end_dt
     }
