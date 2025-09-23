3a093896cb6c22536268d0f9738c1c99aa7085e2
Julie Oh <ohjeoung5224@gmail.com>
2025-09-22 21:39:56 -0700

    Register task blue print in init file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,21 +1,18 @@
 from flask import Flask, jsonify
-from flask_apscheduler import APScheduler
 from flask_cors import CORS
 from user_db.user_db import DatabaseManager
 from user_db.initiate_db import DatabaseSchemaManager
-
+from app.routes_task import tasks_bp
 
 
 app = Flask(__name__)
 
-scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
 
 db = DatabaseManager.connect_to_database()
 schema_manager = DatabaseSchemaManager(db)
 schema_manager.create_all_tables()
 schema_manager.populate_dictionary_tables()
+app.register_blueprint(tasks_bp)
 
 CORS(app)
 from app import routes
